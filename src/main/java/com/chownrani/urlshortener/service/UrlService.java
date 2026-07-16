d9754ca8086f2d18c0ba3c444f147e721a69180a
chownrani <chownrani@proton.me>
2026-07-16 12:01:09 -0300

    feat: url service

---
+++ src/main/java/com/chownrani/urlshortener/service/UrlService.java
@@ -0,0 +1,31 @@
+package com.chownrani.urlshortener.service;
+
+import org.springframework.stereotype.Service;
+
+import com.chownrani.urlshortener.model.UrlModel;
+import com.chownrani.urlshortener.model.utils.ShortCodeGenerator;
+import com.chownrani.urlshortener.repository.UrlRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class UrlService {
+
+    private final UrlRepository repository;
+
+    public String getShortUrl(UrlModel originalUrl) {
+        String shortCode = createShortCode(originalUrl.getId());
+        
+        return UrlModel.BASE_URL + shortCode;
+    }
+
+    public String getOriginalUrlByShortUrl(String shortUrl) {
+        return repository.getOriginalUrlByShortUrl(shortUrl).getOriginalUrl();
+    }
+
+    public String createShortCode(Long urlSeq) {
+        return ShortCodeGenerator.createShortCode(urlSeq);
+    }
+
+}
