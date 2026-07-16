6f1edbac547964a8f05abb48fe8ab832c108fda9
chownrani <chownrani@proton.me>
2026-07-16 12:00:52 -0300

    feat: get original url by short url

--- src/main/java/com/chownrani/urlshortener/repository/UrlRepository.java
+++ src/main/java/com/chownrani/urlshortener/repository/UrlRepository.java
@@ -1,9 +1,10 @@
 package com.chownrani.urlshortener.repository;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.chownrani.urlshortener.model.UrlModel;
 
-public interface UrlRepository extends MongoRepository<UrlModel, ObjectId> {
+public interface UrlRepository extends MongoRepository<UrlModel, Long> {
+
+    UrlModel getOriginalUrlByShortUrl(String shortCode);
 }
