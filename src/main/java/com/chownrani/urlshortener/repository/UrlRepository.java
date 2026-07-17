4a46cab0ddb05c5a304d649096e39475ae97a650
chownrani <chownrani@proton.me>
2026-07-17 16:57:21 -0300

    feat: specific queries

--- src/main/java/com/chownrani/urlshortener/repository/UrlRepository.java
+++ src/main/java/com/chownrani/urlshortener/repository/UrlRepository.java
@@ -1,10 +1,13 @@
 package com.chownrani.urlshortener.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.chownrani.urlshortener.model.UrlModel;
 
 public interface UrlRepository extends MongoRepository<UrlModel, Long> {
 
-    UrlModel getOriginalUrlByShortUrl(String shortCode);
+    Optional<UrlModel> findByShortUrl(String shortUrl);
+    Optional<UrlModel> findByOriginalUrl(String originalUrl);
 }
