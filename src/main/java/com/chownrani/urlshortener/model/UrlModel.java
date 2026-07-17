182a785337829edfd669025d1c43756664e2f97d
chownrani <chownrani@proton.me>
2026-07-17 16:33:44 -0300

    refactor: original url constructor

--- src/main/java/com/chownrani/urlshortener/model/UrlModel.java
+++ src/main/java/com/chownrani/urlshortener/model/UrlModel.java
@@ -3,26 +3,21 @@ package com.chownrani.urlshortener.model;
 import java.time.Instant;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 @Document(collection = "urls")
 public class UrlModel {
-    
-    @Transient
-    public static final String BASE_URL = "https://rani.com/";
 
-    @Transient
     public static final String  SEQUENCE_NAME = "users_sequence";
 
     @Id
@@ -34,7 +29,7 @@ public class UrlModel {
     @Indexed(unique = true)
     private String shortUrl;
 
-    private String originalUrl;
+    private final String originalUrl;
 
     private Instant createdAt;
 
