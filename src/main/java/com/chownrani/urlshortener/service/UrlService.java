99bc75954cb76489e95acdd7f4d9798fff51bd8f
chownrani <chownrani@proton.me>
2026-07-17 16:33:04 -0300

    feat: service methods

--- src/main/java/com/chownrani/urlshortener/service/UrlService.java
+++ src/main/java/com/chownrani/urlshortener/service/UrlService.java
@@ -1,7 +1,9 @@
 package com.chownrani.urlshortener.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.chownrani.urlshortener.exception.UrlNotExistsException;
 import com.chownrani.urlshortener.model.UrlModel;
 import com.chownrani.urlshortener.model.utils.ShortCodeGenerator;
 import com.chownrani.urlshortener.repository.UrlRepository;
@@ -13,16 +15,41 @@ import lombok.AllArgsConstructor;
 public class UrlService {
 
     private final UrlRepository repository;
+
     private final ShortCodeGenerator shortCodeGenerator;
 
-    public String getShortUrl(UrlModel originalUrl) {
-        String shortCode = createShortCode(originalUrl.getId());
-        
-        return UrlModel.BASE_URL + shortCode;
+    private final SequenceGeneratorService sequenceGeneratorService;
+
+    @Value("${base.url}")
+    private String baseUrl;
+
+    public UrlModel create(UrlModel urlModel) {
+        return repository.findByOriginalUrl(urlModel.getOriginalUrl())
+            .orElseGet(() -> {
+                if (urlModel.getId() == null || urlModel.getId() < 1) {
+                    urlModel.setId(
+                        sequenceGeneratorService.generateSequence(UrlModel.SEQUENCE_NAME)
+                    );
+                }
+
+                String shortCode = createShortCode(urlModel.getId());
+                urlModel.setShortCode(shortCode);
+                urlModel.setShortUrl(baseUrl + shortCode);
+                return repository.save(urlModel);
+            });
     }
 
     public String getOriginalUrlByShortUrl(String shortUrl) {
-        return repository.getOriginalUrlByShortUrl(shortUrl).getOriginalUrl();
+        return repository
+            .findByShortUrl(shortUrl)
+            .map(UrlModel::getOriginalUrl)
+            .orElseThrow(() -> new UrlNotExistsException());
+    }
+
+    public String getShortUrl(UrlModel originalUrl) {
+        String shortCode = createShortCode(originalUrl.getId());
+
+        return baseUrl + shortCode;
     }
 
     public String createShortCode(Long urlSeq) {
